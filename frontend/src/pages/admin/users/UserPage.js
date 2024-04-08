036bb6477b6c4d715dd28153d1b53660fef8ed88
Happneel <69533322+Happneel@users.noreply.github.com>
2024-04-08 11:07:33 +0100

    [FIX] Refactor of the style in admin/user & added search box

--- frontend/src/pages/admin/users/UserPage.js
+++ frontend/src/pages/admin/users/UserPage.js
@@ -1,17 +1,20 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 import { AdminContext } from '../../../utils/adminContext';
+import TableTemplate from '../../../components/TableTemplate/TableTemplate';
+import IsLoading from '../../../components/IsLoading/IsLoading';
 import ModalAddUser from './ModalAddUser';
 import ModalEditUser from './ModalEditUser';
 import ModalConfirmation from './ModalConfirmation';
 
 const UserPage = () => {
-    const { users, deleteUser, setMessageNotification } = useContext(AdminContext);
+    const { roles, users, deleteUser, setMessageNotification } = useContext(AdminContext);
     const [showModalAddUser, setShowModalAddUser] = useState(false);
     const [showModalEditUser, setShowModalEditUser] = useState(false);
     const [showModalDeleteUser, setShowModalDeleteUser] = useState(false);
     const [idUserToDelete, setIdUserToDelete] = useState(null);
     const [editUserData, setEditUserData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         // Cette partie pourrait être utilisée pour charger les données depuis un backend
@@ -39,43 +42,120 @@ const UserPage = () => {
         setShowModalDeleteUser(false);
     };
 
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-12 text-center">
-                    <h2>Gestion des Utilisateurs</h2>
+    const header = [
+        {
+            "key": "id",
+            "label": "ID",
+            "edit": false,
+        },
+        {
+            "key": "name",
+            "label": "Nom",
+            "edit": true,
+            "required": true
+        },
+        {
+            "key": "firstname",
+            "label": "Prénom",
+            "edit": true,
+            "required": true
+        },
+        {
+            "key": "email",
+            "label": "Email",
+            "edit": true,
+            "required": false
+        },
+        {
+            "key": "role",
+            "label": "Rôle",
+            "edit": true,
+            "required": true
+        },
+        {
+            "key": "action",
+            "label": "Action",
+            "edit": false,
+            "render": (value, row, index) => (
+                <div className="d-flex gap-2 align-items-center justify-content-center">
+                    <button onClick={() => handleModalEditUserOpen(row)} className="btn d-flex align-items-center justify-content-center"><FaEdit className="action-icon mr-2 text-primary" /></button>
+                    <button onClick={() => handleModalDeleteUserOpen(row.id)} className="btn d-flex align-items-center justify-content-center"><FaTrash className="action-icon text-danger" /></button>
                 </div>
-                <div className="col-12 d-flex justify-content-center mb-3">
-                    <button className="btn btn-primary" onClick={handleModalAddUserOpen}>
+            )
+        }
+    ];
+
+    return (
+        <div className="Content">
+            <h4>Administration : Gestion des Utilisateurs</h4>
+            <form className="row g-3 whiteBox searchBox">
+                <div className="col-md-2">
+                    <label htmlFor="idUser" className="form-label">ID</label>
+                    <input type="text" className="form-control" id="idUser" placeholder="123456" />
+                </div>
+                <div className="col-md-2">
+                    <label htmlFor="nomUser" className="form-label">Nom</label>
+                    <input type="text" className="form-control" id="nomUser" placeholder="Martin" />
+                </div>
+                <div className="col-md-2">
+                    <label htmlFor="prenomUser" className="form-label">Prénom</label>
+                    <input type="text" className="form-control" id="prenomUser" placeholder="Jean" />
+                </div>
+                <div className="col-md-6">
+                    <label htmlFor="mailUser" className="form-label">Mail</label>
+                    <input type="text" className="form-control" id="mailUser" placeholder="martin.jean@mail.com" />
+                </div>
+                <div className="col-md-3">
+                    <label htmlFor="roleUser" className="form-label">Rôle</label>
+                    <select id="roleUser" className="form-select">
+                        <option value="0" disabled selected>Choisir un rôle</option>
+                        {roles.map((role) => (
+                            <option key={role.id} value={role.id}>{role.name}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="col-md-3"></div>
+                <div className="col-md-6 d-flex gap-3 justify-content-end">
+                    <button type="button" className="btn btn-outline-primary w-50 d-none" id='resetButton' onClick={() => {
+                        document.getElementById('idUser').value = '';
+                        document.getElementById('nomUser').value = '';
+                        document.getElementById('prenomUser').value = '';
+                        document.getElementById('mailUser').value = '';
+                        document.getElementById('roleUser').value = '0';
+                        document.getElementById('resetButton').classList.add('d-none');
+                    }}>Réinitialiser</button>
+                    <button type="submit" className="btn btn-primary w-50" onClick={(event) => {
+                        event.preventDefault();
+                        const search = {
+                            id: document.getElementById('idUser').value,
+                            name: document.getElementById('nomUser').value,
+                            firstname: document.getElementById('prenomUser').value,
+                            email: document.getElementById('mailUser').value,
+                            role: document.getElementById('roleUser').value
+                        };
+                        console.log(search);
+                        document.getElementById('resetButton').classList.remove('d-none');
+                    }}>Rechercher</button>
+                </div>
+            </form>
+            <div className='whiteBox gap-4'>
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <h5>Liste des utilisateurs</h5>
+                    <button className="btn btn-outline-primary" onClick={handleModalAddUserOpen}>
                         Ajouter un utilisateur
                     </button>
                 </div>
-            </div>
-            <div className="row justify-content-center">
-                <div className='col-12 col-md-10'>
-                    <div className="table-responsive">
-                        <table className="table table-dark">
-                            <tbody>
-                                {users.map((user, index) => (
-                                    <tr key={index}>
-                                        <td>{user.id}</td>
-                                        <td>{user.name}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.role}</td>
-                                        <td className="text-center">
-                                            <FaEdit onClick={() => handleModalEditUserOpen(user)} className="action-icon mr-2 text-primary" />
-                                            <FaTrash onClick={() => handleModalDeleteUserOpen(user.id)} className="action-icon text-danger" />
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-
-                        </table>
-                    </div>
-                    {showModalAddUser && <ModalAddUser showModal={showModalAddUser} handleModalClose={handleModalAddUserClose} />}
-                    {showModalEditUser && <ModalEditUser userData={editUserData} showModal={showModalEditUser} handleModalClose={handleModalEditUserClose} />}
-                    {showModalDeleteUser && <ModalConfirmation handleConfirm={() => handleConfirmDeleteUser(idUserToDelete)} show={showModalDeleteUser} handleModalClose={handleModalDeleteUserClose} />}
+                <div className="d-flex justify-content-end w-100">
+                    <small>{users.length} utilisateurs trouvés</small>
                 </div>
+                <TableTemplate
+                    theadData={header}
+                    tbodyData={users}
+                    isLoading={isLoading}
+                />
+                {showModalAddUser && <ModalAddUser showModal={showModalAddUser} handleModalClose={handleModalAddUserClose} />}
+                {showModalEditUser && <ModalEditUser userData={editUserData} showModal={showModalEditUser} handleModalClose={handleModalEditUserClose} />}
+                {showModalDeleteUser && <ModalConfirmation handleConfirm={() => handleConfirmDeleteUser(idUserToDelete)} show={showModalDeleteUser} handleModalClose={handleModalDeleteUserClose} />}
             </div>
         </div>
     );
