7a4bfd0c471ec1c17a49bda092f4f3c27ba1386a
Chafernocc <medtakisimo@gmail.com>
2024-04-26 14:21:08 +0200

    essaie test
    Co-Authored-By: Leroc Nathan <84940297+nathan91790@users.noreply.github.com>

---
+++ backend/Res_Rel/src/test/java/com/rr/service/MaListeAmiTest.java
@@ -0,0 +1,32 @@
+package com.rr.service;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+import com.rr.services.MaListeAmi;
+import org.aspectj.lang.annotation.Before;
+import org.testng.annotations.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.rr.entity.Citoyen;
+import com.rr.entity.DemandeAmi;
+import com.rr.repository.DemandeAmiRepository;
+
+public class MaListeAmiTest {
+
+   /* @Mock
+    private DemandeAmiRepository demandeAmiRepository;
+
+    @InjectMocks
+    private MaListeAmi maListeAmi;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }*/
+    @Test
+    public void testdetest(){
+        assertTrue.(true);
+    }
+
+}
